255e94937e0da963b5c053e139a5017a1b5a9c43
kemvl <atdayewkemal@gmail.com>
2024-03-25 15:03:03 +0500

    models are made

--- internal/domain/cinema.go
+++ internal/domain/cinema.go
@@ -1,1 +1,31 @@
 package domain
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type CommonCinemaRequest struct {
+	Cover          string   `json:"cover" bson:"cover"`
+	Type           string   `json:"type" bson:"type"`
+	Name           string   `json:"name" bson:"name"`
+	Location       string   `json:"location" bson:"location"`
+	PhoneNumbers   []string `json:"phone_numbers" bson:"phone_numbers"`
+	Websites       []string `json:"websites" bson:"websites"`
+	SocialNetworks []string `json:"social_networks" bson:"social_networks"`
+	Media          []string `json:"media" bson:"media"`
+}
+
+type CommonCinemaResponse struct {
+	ID             primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Type           string             `json:"type" bson:"type"`
+	Name           string             `json:"name" bson:"name"`
+	Location       string             `json:"location" bson:"location"`
+	PhoneNumbers   []string           `json:"phone_numbers" bson:"phone_numbers"`
+	Websites       []string           `json:"websites" bson:"websites"`
+	SocialNetworks []string           `json:"social_networks" bson:"social_networks"`
+	Media          []string           `json:"media" bson:"media"`
+}
+
+type GetCinemaResponse CommonCinemaResponse
+type CreateCinemaRequest CommonCinemaRequest
+type CreateCinemaResponse CommonCinemaResponse
+type UpdateCinemaRequest CommonCinemaRequest
+type UpdateCinemaResponse CommonCinemaResponse
--- internal/domain/theatre.go
+++ internal/domain/theatre.go
@@ -1,1 +1,32 @@
 package domain
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type CommonTheatreRequest struct {
+	Cover          string   `json:"cover" bson:"cover"`
+	Type           string   `json:"type" bson:"type"`
+	Name           string   `json:"name" bson:"name"`
+	Location       string   `json:"location" bson:"location"`
+	PhoneNumbers   []string `json:"phone_numbers" bson:"phone_numbers"`
+	Websites       []string `json:"websites" bson:"websites"`
+	SocialNetworks []string `json:"social_networks" bson:"social_networks"`
+	Media          []string `json:"media" bson:"media"`
+}
+
+type CommonTheatreResponse struct {
+	ID             primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Cover          string             `json:"cover" bson:"cover"`
+	Type           string             `json:"type" bson:"type"`
+	Name           string             `json:"name" bson:"name"`
+	Location       string             `json:"location" bson:"location"`
+	PhoneNumbers   []string           `json:"phone_numbers" bson:"phone_numbers"`
+	Websites       []string           `json:"websites" bson:"websites"`
+	SocialNetworks []string           `json:"social_networks" bson:"social_networks"`
+	Media          []string           `json:"media" bson:"media"`
+}
+
+type GetTheatreResponse CommonTheatreResponse
+type CreateTheatreRequest CommonTheatreRequest
+type CreateTheatreResponse CommonTheatreResponse
+type UpdateTheatreRequest CommonTheatreRequest
+type UpdateTheatreResponse CommonTheatreResponse
