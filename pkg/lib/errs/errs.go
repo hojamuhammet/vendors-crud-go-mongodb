383ed3f4daf5c428606e4387cc3f5aac90fbe3d6
kemvl <atdayewkemal@gmail.com>
2024-03-27 12:17:40 +0500

    important errors are added

--- pkg/lib/errs/errs.go
+++ pkg/lib/errs/errs.go
@@ -1,1 +1,14 @@
 package errs
+
+const (
+	InvalidRequestFormat = "Invalid request format"
+	InvalidCafeID        = "Invalid cafe id"
+	InvalidPerformanceID = "Invalid performance id"
+	CafeNotFound         = "Cafe not found"
+	PerformanceNotFound  = "Performance not found"
+	InternalServerError  = "Internal server error"
+	InvalidRequestBody   = "Invalid request body"
+	InvalidPage          = "Invalid page"
+	InvalidPageSize      = "Invalid page size"
+	MissingTags          = "Missing tags"
+)
